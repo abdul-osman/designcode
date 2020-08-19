dc75ff0031f0ac20723cb265b1059f4d007698a7
Abdulkadir Osman <abdul.osman87@gmail.com>
2020-08-19 15:28:47 +0100

    added scaffold and safearea

--- designcodeapp/lib/main.dart
+++ designcodeapp/lib/main.dart
@@ -7,8 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // MaterialApp is a widget that provides pre-build widgets are the root of the app 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+    // Scaffold is another widget that instructs tha layout of the app 
       home: Scaffold(
         body: SafeArea(
           child: Text('Hello World'),
